b8eac3448021d2dcb3019b0a5199fbfb61f1f088
RealMarco <yjliu_marco@foxmail.com>
2023-06-08 20:56:12 +0800

    save_demo_fusion_peract

--- tools/dataset_generator.py
+++ tools/dataset_generator.py
@@ -25,6 +25,9 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('save_path',
                     '/tmp/rlbench_data/',
                     'Where to save the demos.')
+flags.DEFINE_string('save_mode',
+                    'fusion_peract',
+                    'fusion_peract save mode only save rgbd for 4 cameras.') # Added
 flags.DEFINE_list('tasks', [],
                   'The tasks to collect. If empty, all tasks are collected.')
 flags.DEFINE_list('image_size', [128, 128],
@@ -170,6 +173,128 @@ def save_demo(demo, example_path, variation):
     with open(os.path.join(example_path, VARIATION_NUMBER), 'wb') as f:
         pickle.dump(variation, f)
 
+def save_demo_fusion_peract(demo, example_path, variation):
+
+    # Save image data first, and then None the image data, and pickle
+    left_shoulder_rgb_path = os.path.join(
+        example_path, LEFT_SHOULDER_RGB_FOLDER)
+    left_shoulder_depth_path = os.path.join(
+        example_path, LEFT_SHOULDER_DEPTH_FOLDER)
+#    left_shoulder_mask_path = os.path.join(
+#        example_path, LEFT_SHOULDER_MASK_FOLDER)
+    right_shoulder_rgb_path = os.path.join(
+        example_path, RIGHT_SHOULDER_RGB_FOLDER)
+    right_shoulder_depth_path = os.path.join(
+        example_path, RIGHT_SHOULDER_DEPTH_FOLDER)
+#    right_shoulder_mask_path = os.path.join(
+#        example_path, RIGHT_SHOULDER_MASK_FOLDER)
+#    overhead_rgb_path = os.path.join(
+#        example_path, OVERHEAD_RGB_FOLDER)
+#    overhead_depth_path = os.path.join(
+#        example_path, OVERHEAD_DEPTH_FOLDER)
+#    overhead_mask_path = os.path.join(
+#        example_path, OVERHEAD_MASK_FOLDER)
+    wrist_rgb_path = os.path.join(example_path, WRIST_RGB_FOLDER)
+    wrist_depth_path = os.path.join(example_path, WRIST_DEPTH_FOLDER)
+#    wrist_mask_path = os.path.join(example_path, WRIST_MASK_FOLDER)
+    front_rgb_path = os.path.join(example_path, FRONT_RGB_FOLDER)
+    front_depth_path = os.path.join(example_path, FRONT_DEPTH_FOLDER)
+#    front_mask_path = os.path.join(example_path, FRONT_MASK_FOLDER)
+
+    check_and_make(left_shoulder_rgb_path)
+    check_and_make(left_shoulder_depth_path)
+#    check_and_make(left_shoulder_mask_path)
+    check_and_make(right_shoulder_rgb_path)
+    check_and_make(right_shoulder_depth_path)
+#    check_and_make(right_shoulder_mask_path)
+#    check_and_make(overhead_rgb_path)
+#    check_and_make(overhead_depth_path)
+#    check_and_make(overhead_mask_path)
+    check_and_make(wrist_rgb_path)
+    check_and_make(wrist_depth_path)
+#    check_and_make(wrist_mask_path)
+    check_and_make(front_rgb_path)
+    check_and_make(front_depth_path)
+#    check_and_make(front_mask_path)
+
+    for i, obs in enumerate(demo):
+        left_shoulder_rgb = Image.fromarray(obs.left_shoulder_rgb)
+        left_shoulder_depth = utils.float_array_to_rgb_image(
+            obs.left_shoulder_depth, scale_factor=DEPTH_SCALE)
+#        left_shoulder_mask = Image.fromarray(
+#            (obs.left_shoulder_mask * 255).astype(np.uint8))
+        right_shoulder_rgb = Image.fromarray(obs.right_shoulder_rgb)
+        right_shoulder_depth = utils.float_array_to_rgb_image(
+            obs.right_shoulder_depth, scale_factor=DEPTH_SCALE)
+#        right_shoulder_mask = Image.fromarray(
+#            (obs.right_shoulder_mask * 255).astype(np.uint8))
+#        overhead_rgb = Image.fromarray(obs.overhead_rgb)
+#        overhead_depth = utils.float_array_to_rgb_image(
+#            obs.overhead_depth, scale_factor=DEPTH_SCALE)
+#        overhead_mask = Image.fromarray(
+#            (obs.overhead_mask * 255).astype(np.uint8))
+        wrist_rgb = Image.fromarray(obs.wrist_rgb)
+        wrist_depth = utils.float_array_to_rgb_image(
+            obs.wrist_depth, scale_factor=DEPTH_SCALE)
+#        wrist_mask = Image.fromarray((obs.wrist_mask * 255).astype(np.uint8))
+        front_rgb = Image.fromarray(obs.front_rgb)
+        front_depth = utils.float_array_to_rgb_image(
+            obs.front_depth, scale_factor=DEPTH_SCALE)
+#        front_mask = Image.fromarray((obs.front_mask * 255).astype(np.uint8))
+
+        left_shoulder_rgb.save(
+            os.path.join(left_shoulder_rgb_path, IMAGE_FORMAT % i))
+        left_shoulder_depth.save(
+            os.path.join(left_shoulder_depth_path, IMAGE_FORMAT % i))
+#        left_shoulder_mask.save(
+#            os.path.join(left_shoulder_mask_path, IMAGE_FORMAT % i))
+        right_shoulder_rgb.save(
+            os.path.join(right_shoulder_rgb_path, IMAGE_FORMAT % i))
+        right_shoulder_depth.save(
+            os.path.join(right_shoulder_depth_path, IMAGE_FORMAT % i))
+#        right_shoulder_mask.save(
+#            os.path.join(right_shoulder_mask_path, IMAGE_FORMAT % i))
+#        overhead_rgb.save(
+#            os.path.join(overhead_rgb_path, IMAGE_FORMAT % i))
+#        overhead_depth.save(
+#            os.path.join(overhead_depth_path, IMAGE_FORMAT % i))
+#        overhead_mask.save(
+#            os.path.join(overhead_mask_path, IMAGE_FORMAT % i))
+        wrist_rgb.save(os.path.join(wrist_rgb_path, IMAGE_FORMAT % i))
+        wrist_depth.save(os.path.join(wrist_depth_path, IMAGE_FORMAT % i))
+#        wrist_mask.save(os.path.join(wrist_mask_path, IMAGE_FORMAT % i))
+        front_rgb.save(os.path.join(front_rgb_path, IMAGE_FORMAT % i))
+        front_depth.save(os.path.join(front_depth_path, IMAGE_FORMAT % i))
+#        front_mask.save(os.path.join(front_mask_path, IMAGE_FORMAT % i))
+
+        # We save the images separately, so set these to None for pickling.
+        obs.left_shoulder_rgb = None
+        obs.left_shoulder_depth = None
+        obs.left_shoulder_point_cloud = None
+        obs.left_shoulder_mask = None
+        obs.right_shoulder_rgb = None
+        obs.right_shoulder_depth = None
+        obs.right_shoulder_point_cloud = None
+        obs.right_shoulder_mask = None
+        obs.overhead_rgb = None
+        obs.overhead_depth = None
+        obs.overhead_point_cloud = None
+        obs.overhead_mask = None
+        obs.wrist_rgb = None
+        obs.wrist_depth = None
+        obs.wrist_point_cloud = None
+        obs.wrist_mask = None
+        obs.front_rgb = None
+        obs.front_depth = None
+        obs.front_point_cloud = None
+        obs.front_mask = None
+
+    # Save the low-dimension data
+    with open(os.path.join(example_path, LOW_DIM_PICKLE), 'wb') as f:
+        pickle.dump(demo, f)
+
+    with open(os.path.join(example_path, VARIATION_NUMBER), 'wb') as f:
+        pickle.dump(variation, f)
 
 def run(i, lock, task_index, variation_count, results, file_lock, tasks):
     """Each thread will choose one task and variation, and then gather
@@ -285,7 +410,10 @@ def run(i, lock, task_index, variation_count, results, file_lock, tasks):
                     break
                 episode_path = os.path.join(episodes_path, EPISODE_FOLDER % ex_idx)
                 with file_lock:
-                    save_demo(demo, episode_path, my_variation_count)
+                    if FLAGS.save_mode == 'fusion_peract':
+                        save_demo_fusion_peract(demo, episode_path, my_variation_count) # Added, for fusion_peract
+                    else:
+                        save_demo(demo, episode_path, my_variation_count)
 
                     with open(os.path.join(
                             episode_path, VARIATION_DESCRIPTIONS), 'wb') as f:
@@ -399,7 +527,10 @@ def run_all_variations(i, lock, task_index, variation_count, results, file_lock,
                     break
                 episode_path = os.path.join(episodes_path, EPISODE_FOLDER % ex_idx)
                 with file_lock:
-                    save_demo(demo, episode_path, variation)
+                    if FLAGS.save_mode == 'fusion_peract':
+                        save_demo_fusion_peract(demo, episode_path, variation)  # Added, for fusion_peract
+                    else:
+                        save_demo(demo, episode_path, variation)
 
                     with open(os.path.join(
                             episode_path, VARIATION_DESCRIPTIONS), 'wb') as f:
